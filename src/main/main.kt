6d8426dcef126eef1a7835201b1fb6950f469aa3
PGAT begginer <137990341+hachchch@users.noreply.github.com>
2023-11-23 18:53:42 +0900

    Create main.kt

---
+++ src/main/main.kt
@@ -0,0 +1,13 @@
+package com.unciv.build
+
+object BuildConfig {
+    const val kotlinVersion = "1.8.21"
+    const val appName = "Unciv"
+    const val appCodeNumber = 931
+    const val appVersion = "4.9.1"
+
+    const val gdxVersion = "1.12.1"
+    const val ktorVersion = "2.2.3"
+    const val coroutinesVersion = "1.7.2"
+    const val roboVMVersion = "2.3.1"
+}
